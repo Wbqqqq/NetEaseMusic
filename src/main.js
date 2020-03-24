63da27eb8c2cedac8637f7883d2dee2e8acc25f7
汪炳权 <353351363@qq.com>
2020-03-24 18:07:10 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fastclick from 'fastclick'
+import './assets/css/base.scss'
+
+// 解决移动端100-300毫秒延迟问题
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
